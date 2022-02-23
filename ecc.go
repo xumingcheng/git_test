fdc1bd76657fbc3f32cc1448f33ffed05812ed5b
xumingcheng <62745635+xumingcheng@users.noreply.github.com>
2022-02-23 19:41:21 +0800

    Update ecc.go

--- ecc.go
+++ ecc.go
@@ -59,3 +59,125 @@ d,e:=params3.Add(*d,*e,*d,*e)
 	end:=time.Since(start)
 	fmt.Printf("abv%v",end )
 }
+func (p *pbft) handleClientRequest(content []byte) {
+	fmt.Println("主节点已接收到客户端发来的request ...")
+	//使用json解析出Request结构体
+	r := new(Request)
+	err := json.Unmarshal(content, r)
+	if err != nil {
+		log.Panic(err)
+	}
+	//添加信息序号
+	p.sequenceIDAdd()
+	//获取消息摘要
+	digest := getDigest(*r)
+	fmt.Println("已将request存入临时消息池")
+	//存入临时消息池
+	p.messagePool[digest] = *r
+	//主节点对消息摘要进行签名
+	digestByte, _ := hex.DecodeString(digest)
+	signInfo := p.RsaSignWithSha256(digestByte, p.node.rsaPrivKey)
+	//拼接成PrePrepare，准备发往follower节点
+	pp := PrePrepare{*r, digest, p.sequenceID, signInfo}
+	b, err := json.Marshal(pp)
+	if err != nil {
+		log.Panic(err)
+	}
+	fmt.Println("正在向其他节点进行进行PrePrepare广播 ...")
+	//进行PrePrepare广播
+	p.broadcast(cPrePrepare, b)
+	fmt.Println("PrePrepare广播完成")
+}
+
+//处理预准备消息
+func (p *pbft) handlePrePrepare(content []byte) {
+	fmt.Println("本节点已接收到主节点发来的PrePrepare ...")
+	//	//使用json解析出PrePrepare结构体
+	pp := new(PrePrepare)
+	err := json.Unmarshal(content, pp)
+	if err != nil {
+		log.Panic(err)
+	}
+	//获取主节点的公钥，用于数字签名验证
+	primaryNodePubKey := p.getPubKey("N0")
+	digestByte, _ := hex.DecodeString(pp.Digest)
+	if digest := getDigest(pp.RequestMessage); digest != pp.Digest {
+		fmt.Println("信息摘要对不上，拒绝进行prepare广播")
+	} else if p.sequenceID+1 != pp.SequenceID {
+		fmt.Println("消息序号对不上，拒绝进行prepare广播")
+	} else if !p.RsaVerySignWithSha256(digestByte, pp.Sign, primaryNodePubKey) {
+		fmt.Println("主节点签名验证失败！,拒绝进行prepare广播")
+	} else {
+		//序号赋值
+		p.sequenceID = pp.SequenceID
+		//将信息存入临时消息池
+		fmt.Println("已将消息存入临时节点池")
+		p.messagePool[pp.Digest] = pp.RequestMessage
+		//节点使用私钥对其签名
+		sign := p.RsaSignWithSha256(digestByte, p.node.rsaPrivKey)
+		//拼接成Prepare
+		pre := Prepare{pp.Digest, pp.SequenceID, p.node.nodeID, sign}
+		bPre, err := json.Marshal(pre)
+		if err != nil {
+			log.Panic(err)
+		}
+		//进行准备阶段的广播
+		fmt.Println("正在进行Prepare广播 ...")
+		p.broadcast(cPrepare, bPre)
+		fmt.Println("Prepare广播完成")
+	}
+}
+
+//处理准备消息
+func (p *pbft) handlePrepare(content []byte) {
+	//使用json解析出Prepare结构体
+	pre := new(Prepare)
+	err := json.Unmarshal(content, pre)
+	if err != nil {
+		log.Panic(err)
+	}
+	fmt.Printf("本节点已接收到%s节点发来的Prepare ... \n", pre.NodeID)
+	//获取消息源节点的公钥，用于数字签名验证
+	MessageNodePubKey := p.getPubKey(pre.NodeID)
+	digestByte, _ := hex.DecodeString(pre.Digest)
+	if _, ok := p.messagePool[pre.Digest]; !ok {
+		fmt.Println("当前临时消息池无此摘要，拒绝执行commit广播")
+	} else if p.sequenceID != pre.SequenceID {
+		fmt.Println("消息序号对不上，拒绝执行commit广播")
+	} else if !p.RsaVerySignWithSha256(digestByte, pre.Sign, MessageNodePubKey) {
+		fmt.Println("节点签名验证失败！,拒绝执行commit广播")
+	} else {
+		p.setPrePareConfirmMap(pre.Digest, pre.NodeID, true)
+		count := 0
+		for range p.prePareConfirmCount[pre.Digest] {
+			count++
+		}
+		//因为主节点不会发送Prepare，所以不包含自己
+		specifiedCount := 0
+		if p.node.nodeID == "N0" {
+			specifiedCount = nodeCount / 3 * 2
+		} else {
+			specifiedCount = (nodeCount / 3 * 2) - 1
+		}
+		//如果节点至少收到了2f个prepare的消息（包括自己）,并且没有进行过commit广播，则进行commit广播
+		p.lock.Lock()
+		//获取消息源节点的公钥，用于数字签名验证
+		if count >= specifiedCount && !p.isCommitBordcast[pre.Digest] {
+			fmt.Println("本节点已收到至少2f个节点(包括本地节点)发来的Prepare信息 ...")
+			//节点使用私钥对其签名
+			sign := p.RsaSignWithSha256(digestByte, p.node.rsaPrivKey)
+			c := Commit{pre.Digest, pre.SequenceID, p.node.nodeID, sign}
+			bc, err := json.Marshal(c)
+			if err != nil {
+				log.Panic(err)
+			}
+			//进行提交信息的广播
+			fmt.Println("正在进行commit广播")
+			p.broadcast(cCommit, bc)
+			p.isCommitBordcast[pre.Digest] = true
+			fmt.Println("commit广播完成")
+		}
+		p.lock.Unlock()
+	}
+}
+
